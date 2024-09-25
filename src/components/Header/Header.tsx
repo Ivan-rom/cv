6a6865ec5a830eadb9e09904fc86c942d093fc9f
Ivan-rom <rom_1van@icloud.com>
2024-09-25 19:48:15 +0600

    feat: implement animation for header on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Container from "../Container/Container";
 import LocaleSelect from "../LocaleSelect/LocaleSelect";
 import Navbar from "../Navbar/Navbar";
 import styles from "./header.module.css";
+import classNames from "classnames";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const headerClasses = {
+    [styles.header]: true,
+    [styles.scrolled]: isScrolled,
+  };
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={classNames(headerClasses)}>
       <Container>
         <div className={styles.content}>
           <Navbar />
