b39ecd3f9fa2c802289a926f0ac507facbeb0cc3
Ivan-rom <rom_1van@icloud.com>
2024-09-28 10:18:30 +0600

    feat: minor updates

--- src/components/LocaleSelect/LocaleSelect.tsx
+++ src/components/LocaleSelect/LocaleSelect.tsx
@@ -2,7 +2,7 @@
 
 import { locales } from "@/helpers/constants";
 import { usePathname } from "@/i18n/routing";
-import { useParams, useRouter } from "next/navigation";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
 import { ChangeEvent, useTransition } from "react";
 import styles from "./localeSelect.module.css";
 
@@ -10,13 +10,16 @@ function LocaleSelect() {
   const router = useRouter();
   const { locale } = useParams();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   const [isPending, startTransition] = useTransition();
 
   const changeHandler = ({ target }: ChangeEvent<HTMLSelectElement>) => {
+    const params = searchParams.toString();
+
     const newLocale = target.value;
     startTransition(() => {
-      router.replace(`/${newLocale}${pathname}`);
+      router.replace(`/${newLocale}${pathname}?${params}`);
     });
   };
 
--- src/components/ProjectsFilters/ProjectsFilters.tsx
+++ src/components/ProjectsFilters/ProjectsFilters.tsx
@@ -1,13 +1,16 @@
 import { SkillsList } from "@/helpers/enums";
 import styles from "./projectsFilter.module.css";
 import Filter from "../Filter/Filter";
+import { useTranslations } from "next-intl";
 
 const filters = Object.values(SkillsList);
 
 function ProjectsFilters() {
+  const t = useTranslations("PortfolioPage.filters");
+
   return (
     <>
-      <h3 className={styles.title}>Filters</h3>
+      <h3 className={styles.title}>{t("title")}</h3>
       <ul className={styles.filters}>
         {filters.map((filter) => (
           <li key={filter}>
